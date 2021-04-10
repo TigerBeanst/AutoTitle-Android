c66aeb59356226fbc4ebe18d6114aee29b8c2b20
hjthjthjt <i@jakting.com>
2021-04-10 09:25:39 +0800

    修复：憨批的状态栏高度导致动画效果有误/添加字符串

--- app/src/main/java/com/jakting/autotitle/SplashActivity.kt
+++ app/src/main/java/com/jakting/autotitle/SplashActivity.kt
@@ -1,5 +1,6 @@
 package com.jakting.autotitle
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -8,14 +9,18 @@ import android.view.View
 import android.view.animation.AccelerateInterpolator
 import android.view.animation.AlphaAnimation
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.gson.Gson
+import com.jakting.autotitle.api.data.LoginStatus
 import com.jakting.autotitle.api.data.TokenBody
-import com.jakting.autotitle.api.data.UserInfo
-import com.jakting.autotitle.utils.*
-import com.jakting.rn6pan.api.accessAPI
+import com.jakting.autotitle.api.parse.getTokenBodyMethod
+import com.jakting.autotitle.api.parse.getUserInfoMethod
+import com.jakting.autotitle.utils.MyApplication.Companion.loginStatus
+import com.jakting.autotitle.utils.MyApplication.Companion.sp
+import com.jakting.autotitle.utils.MyApplication.Companion.spe
+import com.jakting.autotitle.utils.MyApplication.Companion.tokenBody
+import com.jakting.autotitle.utils.RetrofitCallback
+import com.jakting.autotitle.utils.tools.*
 import kotlinx.android.synthetic.main.activity_splash.*
-import okhttp3.MediaType
-import okhttp3.RequestBody
 
 
 class SplashActivity : AppCompatActivity() {
@@ -26,40 +31,59 @@ class SplashActivity : AppCompatActivity() {
         setContentView(R.layout.activity_splash)
         initView()
         Handler(Looper.getMainLooper()).postDelayed({
-            var status_bar_height = 0
-            val resourceId =
-                applicationContext.resources.getIdentifier("status_bar_height", "dimen", "android")
-            if (resourceId > 0) {
-                status_bar_height = applicationContext.resources.getDimensionPixelSize(resourceId)
-            }
-            logd(status_bar_height.toString())
-            val locationIcon = IntArray(2)
-            login_app_icon.getLocationInWindow(locationIcon)
-            val xIcon: Float = locationIcon[0].toFloat()
-            val yIcon: Float = locationIcon[1].toFloat()
-            val targetXIcon = this.dip2px(50f)
-            val targetYIcon = this.dip2px(210f) + status_bar_height
-            login_app_icon.animate()
-                .translationX(-(xIcon - targetXIcon))
-                .translationY(-(yIcon - targetYIcon))
-                .setDuration(1500)
-                .setInterpolator(AccelerateInterpolator())
-                .withLayer()
-            val locationName = IntArray(2)
-            login_app_name.getLocationInWindow(locationName)
-            val xName: Float = locationName[0].toFloat()
-            val yName: Float = locationName[1].toFloat()
+            val loginStatusString = sp.getString("login_status", "")
+            if (loginStatusString != "") {
+                //说明有登录数据（但 access_token 不一定有效）
+                loginStatus =
+                    Gson().fromJson(loginStatusString, LoginStatus::class.java)
+                tokenBody =
+                    Gson().fromJson(sp.getString("token_body", ""), TokenBody::class.java)
+                intentToMainActivity()
+            } else {
+                //还没登陆，触发动画
+                var statusBarHeight = 0
+                val resourceId =
+                    applicationContext.resources.getIdentifier(
+                        "status_bar_height",
+                        "dimen",
+                        "android"
+                    )
+                if (resourceId > 0) {
+                    statusBarHeight = applicationContext.resources.getDimensionPixelSize(resourceId)
+                }
+                logd(statusBarHeight.toString())
+                val locationIcon = IntArray(2)
+                login_app_icon.getLocationInWindow(locationIcon)
+                val xIcon: Float = locationIcon[0].toFloat()
+                val yIcon: Float = locationIcon[1].toFloat()
+                val targetXIcon = this.dip2px(50f)
+                val targetYIcon = this.dip2px(210f) + statusBarHeight
+                login_app_icon.animate()
+                    .translationX(-(xIcon - targetXIcon))
+                    .translationY(-(yIcon - targetYIcon))
+                    .setDuration(1500)
+                    .setInterpolator(AccelerateInterpolator())
+                    .withLayer()
+                val locationName = IntArray(2)
+                login_app_name.getLocationInWindow(locationName)
+                val xName: Float = locationName[0].toFloat()
+                val yName: Float = locationName[1].toFloat()
 
-            val targetXName = this.dip2px(50f)
-            val targetYName = this.dip2px(270f) + status_bar_height
-            login_app_name.animate()
-                .translationX(-(xName - targetXName))
-                .translationY(-(yName - targetYName))
-                .setDuration(1500)
-                .setInterpolator(AccelerateInterpolator())
-                .withLayer()
-            showEverything()
+                val targetXName = this.dip2px(50f)
+                val targetYName = this.dip2px(270f) + statusBarHeight
+                login_app_name.animate()
+                    .translationX(-(xName - targetXName))
+                    .translationY(-(yName - targetYName))
+                    .setDuration(1500)
+                    .setInterpolator(AccelerateInterpolator())
+                    .withLayer()
+                showEverything()
+            }
         }, 1000)
+    }
+
+    private fun intentToMainActivity() {
+        startActivity(Intent(this, MainActivity::class.java))
     }
 
     private fun showEverything() {
@@ -82,60 +106,38 @@ class SplashActivity : AppCompatActivity() {
                     toast(getString(R.string.login_toast_login_empty_password))
                 }
                 else -> {
-                    getTokenBody(usernameInput, passwordInput)
+                    loginStatus.username = Base64Util.encode(usernameInput)
+                    loginStatus.password = Base64Util.encode(passwordInput)
+                    logd("loginStatus.username = ${loginStatus.username}      loginStatus.password = ${loginStatus.password}")
+                    spe.putString("login_status", Gson().toJson(loginStatus))
+                    spe.apply()
+                    getTokenBodyMethod(usernameInput, passwordInput, object : RetrofitCallback {
+                        override fun onSuccess(value: Any) {
+                            tokenBody = value as TokenBody
+                            spe.putString("token_body", Gson().toJson(tokenBody))
+                            spe.apply()
+                            getUserInfoMethod(object : RetrofitCallback {
+                                override fun onSuccess(value: Any) {
+                                    intentToMainActivity()
+                                }
+
+                                override fun onError(t: Throwable) {
+                                    TODO("Not yet implemented")
+                                }
+
+                            })
+                        }
+
+                        override fun onError(t: Throwable) {
+                            if (getErrorStatusCode(t) == 401) {
+                                toast(getErrorString(t))
+                            }
+                        }
+                    })
                 }
             }
         }
     }
 
-    /**
-     * 获取 access_token
-     * @param username String
-     * @param password String
-     */
-    private fun getTokenBody(username: String, password: String) {
-        val createDestinationPostBody: RequestBody =
-            RequestBody.create(
-                MediaType.parse("application/json"),
-                "{\"username\":\"$username\"," +
-                        "\"password\":\"${md5(password)}\"}"
-            )
-        accessAPI(
-            {
-                getToken(createDestinationPostBody)
-            }, { objectReturn ->
-                val tokenBody = objectReturn as TokenBody
-                BaseActivity.access_token = tokenBody.access_token
-                getUserInfo()
-
-            }) { t ->
-            logd("onError // getUserInfo")
-            t.printStackTrace()
-        }
-    }
-
-    /**
-     * 获取用户信息
-     * @param username String
-     * @param password String
-     * @return UserInfo
-     */
-    private fun getUserInfo() {
-        accessAPI(
-            {
-                getUserInfo(bearer(BaseActivity.access_token))
-            }, { objectReturn ->
-                BaseActivity.userInfo = objectReturn as UserInfo
-                toast(getString(R.string.login_toast_login_success))
-                MaterialAlertDialogBuilder(this)
-                    .setTitle("测试")
-                    .setMessage("用户名：${BaseActivity.userInfo.username}\n昵称：${BaseActivity.userInfo.nickname}\n邮箱：${BaseActivity.userInfo.email}\n")
-                    .show()
-
-            }) { t ->
-            logd("onError // getUserInfo")
-            t.printStackTrace()
-        }
-    }
 
 }
